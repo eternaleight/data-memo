38aaeec7c76636025534c5a1a97b44a412df40ef
eternaleight <eternaleight0@gmail.com>
2022-09-05 04:44:02 +0900

    feat(axios): add axiosClient interceptor request

---
+++ src/api/axiosclient.tsx
@@ -0,0 +1,19 @@
+import axios from "axios"
+
+const BASE_URL = "http://localhost:5001/api/v1"
+
+const axiosClient = axios.create({
+  baseURL: BASE_URL,
+})
+const getToken = () => localStorage.getItem("token")
+//APIを叩く前に前処理を行う
+axiosClient.interceptors.request.use(async(config) => {
+  return {
+    config,
+    headers: {
+      "Content-Type": "application/json",
+      authorization: `Bearer ${getToken()}`, //リクエストヘッダにJWTを付けてサーバーに渡す
+    }
+  }
+})
+
