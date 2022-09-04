4e75f0e25b62dc5f7e204745ad2ee17ead0a3d12
eternaleight <eternaleight0@gmail.com>
2022-09-05 02:41:49 +0900

    feat(mui): mui theme color

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,28 @@ import "./App.css"
 import Login from "./pages/login"
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import AuthLayout from "./components/layout/authLayout"
+import { createTheme, ThemeProvider } from "@mui/material/styles"
 import Register from "./pages/register"
+import { blue } from "@mui/material/colors"
+import { CssBaseline } from "@mui/material"
 
 function App() {
+  const theme = createTheme({
+    palette: { primary: blue },
+  })
+
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<AuthLayout />}>
-          <Route path="login" element={<Login />} />
-          <Route path="register" element={<Register />} />
-        </Route>
-      </Routes>
-    </Router>
+    <ThemeProvider theme={theme}>
+      <CssBaseline/>
+      <Router>
+        <Routes>
+          <Route path="/" element={<AuthLayout />}>
+            <Route path="login" element={<Login />} />
+            <Route path="register" element={<Register />} />
+          </Route>
+        </Routes>
+      </Router>
+    </ThemeProvider>
   )
 }
 
