6cf7affb1202546089e864a92e1ca289b4ae99f3
eternaleight <eternaleight0@gmail.com>
2022-09-05 03:48:11 +0900

    feat(register): add textFiled box

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,10 +1,55 @@
+import { LoadingButton } from "@mui/lab"
+import { Box, Button, TextField } from "@mui/material"
 import React from "react"
+import { Link } from "react-router-dom"
 
 const Register = () => {
   return (
-    <div>
-      Register
-    </div>
+    <>
+      <Box component="form">
+        <TextField
+          fullWidth
+          id="username"
+          label="お名前"
+          margin="normal"
+          name="username"
+          required
+        />
+        <TextField
+          fullWidth
+          id="password"
+          label="パスワード"
+          margin="normal"
+          name="password"
+          required
+          type="password"
+          className=""
+        />
+        <TextField
+          fullWidth
+          id="confirmPassword"
+          label="確認用パスワード"
+          margin="normal"
+          name="confirmPassword"
+          required
+          type="password"
+          className=""
+        />
+        <LoadingButton
+          sx={{ mt: 3, mb: 2 }}
+          fullWidth
+          type="submit"
+          loading={false}
+          color="primary"
+          variant="outlined"
+        >
+          アカウント作成
+        </LoadingButton>
+      </Box>
+      <Button component={Link} to="/login">
+        すでにアカウントを持っていますか？ ログイン
+      </Button>
+    </>
   )
 }
 
