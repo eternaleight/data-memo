e71b03d319847ede51028e143abd363c91e88761
eternaleight <eternaleight0@gmail.com>
2022-09-11 03:20:04 +0900

    fix(proxy): change proxy

--- src/api/axiosclient.tsx
+++ src/api/axiosclient.tsx
@@ -1,6 +1,9 @@
 import axios from "axios"
 
-const BASE_URL = "https://n-backend-production.up.railway.app/api/v1"
+const BASE_URL = "https://n-backend.onrender.com/api/v1"
+// const BASE_URL = "http://localhost:5001/api/v1"
+// const BASE_URL = "https://n-backend-production.up.railway.app/api/v1"
+// const BASE_URL = "https://brick-red-pelican-wear.cyclic.app/api/v1"
 
 const axiosClient = axios.create({
   baseURL: BASE_URL,
